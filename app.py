bb921b229d8a4aa0ab7c183eb6a329704792972c
Yuen Wee Kin Edwin <edwin.edwin313@outlook.com>
2024-12-29 23:42:48 +0800

    bootstrap 5 done.

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 @app.route("/")
 def index():
     name = "Edwin"
     return render_template("index.html", person=name)
+
 
 @app.route("/about")
 def about():
